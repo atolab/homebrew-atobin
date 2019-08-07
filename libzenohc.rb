1bd8bb2744b3ca930aaba1488255247afcb2d266
IVAN DARIO PAEZ ANAYA <ivan.paez@adlinktech.com>
2019-08-07 22:55:45 +0200

    Create libzenohc.rb formula

---
+++ libzenohc.rb
@@ -0,0 +1,11 @@
+class Libzenohc < Formula
+  desc "Zenoh-c API."
+  homepage "https://github.com/atolab/zenoh-c"
+  url "https://github.com/atolab/atobin/blob/master/zenoh-c/latest/macos/10.15.5/libzenohc.dylib?raw=true"
+  sha256 "9b967ad687eade6c9f648ab3cfc6fb68640b32fab92fcdfeda34d6ce3f33d9ed"
+  version "0.3.0"
+
+  def install
+    bin.install "libzenohc"
+  end
+end
