4e1f4218f38035673d0eabf948b6b2b8c54b85cd
Olivier Hécart <olivier.hecart@adlinktech.com>
2019-10-09 15:17:24 +0200

    Update

--- zenoh-http.rb
+++ zenoh-http.rb
@@ -2,7 +2,7 @@ class ZenohHttp < Formula
   desc "Zenoh http plugin."
   homepage "https://github.com/atolab/zenoh"
   url "https://github.com/atolab/atobin/raw/master/zenoh-http/unstable/macos/10.14.6/zenoh-http-plugin.cmxs"
-  sha256 "4d43d7c13b746808027990065b7d327474f501782abc3eb083bb0ce020355429"
+  sha256 "edc94f19a1977199b47ffdf05fb72ee8d1d55052f05319bd0f32e3ade30dab60"
   version "0.3.0"
   depends_on "zenohd"
 
--- zenohd.rb
+++ zenohd.rb
@@ -2,7 +2,7 @@ class Zenohd < Formula
   desc "Zenoh daemon."
   homepage "https://github.com/atolab/zenoh"
   url "https://github.com/atolab/atobin/raw/master/zenoh/unstable/macos/10.14.6/zenohd"
-  sha256 "33d9392681e2c460f390a22fb07fbc84e77fdcbe4db28dc4d7aa3e40f18c77b2"
+  sha256 "071d304123ca17b3c686ab62c85cfa3688eec54e5fe89259d1a442a33d7d1fdd"
   version "0.3.0"
 
   depends_on "libev"
